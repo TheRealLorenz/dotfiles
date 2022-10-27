70c72eed40e1159f0ffe1ff05ff78c5ad8a8a3da
TheRealLorenz <lollobellina@gmail.com>
2022-10-27 21:40:05 +0200

    Remove nvim config

--- nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require('user.options')
-require('user.plugins')
-require('user.lsp')
-require('user.cmp')
-require('user.which-key')
-require('user.keymaps')
--- nvim/lua/user/lsp/servers/cssls.lua
+++
@@ -1,6 +0,0 @@
-local capabilities = require("user.lsp.settings").capabilities
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-return {
-  capabilities = capabilities,
-}
