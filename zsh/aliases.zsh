f0f5217527d6ce001b6c9be09e72efb56b3d8081
TheRealLorenz <lollobellina@gmail.com>
2023-02-25 17:25:17 +0100

    [zsh] Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,11 +6,8 @@ optional_alias cat bat
 optional_alias ls exa
 
 alias ll='ls -l'
-#alias lf=lfrun
 alias mkdir='mkdir -pv'
 alias pong='ping google.com'
-alias xclipout='xclip -o -selection clipboard'
-alias xclipin='xclip -i -selection clipboard'
 alias ~='cd ~'
 alias cd..='cd ..'
 alias ..='cd ..'
@@ -25,11 +22,24 @@ alias chgrp='chgrp --preserve-root'
 alias wget='wget -c'
 alias ssh="TERM=xterm-256color ssh"
 alias bar="watch progress -w"
-alias virsh="virsh --connect qemu:///system"
-alias ide="tmux split-window -v -p 30"
 alias rm="printf 'vuoi sfondare tutto di nuovo?\n'"
 alias cdtemp="cd $(mktemp -d)"
 
-function cdp() {
-	cd $(ls -d */ | fzf)
+function cdf() { cd $(ls -d */ | fzf) }
+
+alias s="source ~/.zshrc"
+
+alias gcm="git commit"
+alias gcma="git commit --amend"
+alias glo="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias glop="glo | fzf | cut -d '*' -f2- | cut -d ' ' -f2 | wl-copy"
+alias gst="git status"
+
+function gcmf() {
+    SUMMARY=$(gum input --placeholder "Summary of this change")
+    DESCRIPTION=$(gum write --placeholder "Details of this change (CTRL+D to finish)")
+
+    gum confirm "Commit changes?" \
+        && git commit -m "$SUMMARY" -m "$DESCRIPTION" \
+        || echo "Aborted"
 }
