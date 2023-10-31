b1a74c7c8381565845d35ff07d0c59d5a8dfc867
TheRealLorenz <lollobellina@gmail.com>
2023-10-31 17:15:19 +0100

    [zsh] Template function to support multiple templates at once

--- zsh/templates.zsh
+++ zsh/templates.zsh
@@ -2,16 +2,20 @@
 function template() {
   FILENAME=$1
 
-  if [[ -z ${FILENAME} ]]; then
-    echo "Usage: template TEMPLATE_NAME"
+  if [[ $# -eq 0 ]]; then
+    echo "Usage: template [template_name ...]"
     return 1
   fi
 
-  if [[ ! -f "${HOME}/.config/templates/${FILENAME}" ]]; then
-    echo "Template '$FILENAME' not found"
-    return 1
-  fi
+  for FILENAME in $@
+  do
+    if [[ ! -f "${HOME}/.config/templates/${FILENAME}" ]]; then
+      echo "Template '$FILENAME' not found"
+      return 1
+    fi
 
-  echo "Copying '${FILENAME}' to '${PWD}/'"
-  cp "${HOME}/.config/templates/${FILENAME}" "${PWD}"/
+    echo "Copying '${FILENAME}' to '${PWD}/'"
+    cp "${HOME}/.config/templates/${FILENAME}" "${PWD}"/
+  done
+
 }
